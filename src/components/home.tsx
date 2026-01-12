4ee89148801fa85ca5b33034f3b8550a903f68ae
JanPauldeVoor <jdevoor@clemson.edu>
2026-01-12 15:03:23 -0500

    layout fix

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -29,25 +29,32 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import dash from "../static/fsae-dash.png"
 import zat from "../static/zat.png"
 import avatarImg from "../static/me.png"
+import Grid from "@mui/material/Grid"
 
 
 const AboutMeContainer = styled(Stack)(({ theme }) => ({
   // height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
   // minHeight: "100%",
+  position: 'relative',
+  minHeight: '100vn',
+  width: '100%',
   padding: theme.spacing(4),
   [theme.breakpoints.up("sm")]: {
     padding: theme.spacing(2),
   },
   "&::before": {
     content: '""',
-    // display: "block",
     position: 'absolute',
-    display: 'flex',
-    height: '100%',
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+    // display: 'flex',
+    // height: '100%',
     zIndex: -1,
-    flexGrow: 1,
-    inset: 0,
-    minHeight: '100vh',
+    // flexGrow: 1,
+    // inset: 0,
+    // minHeight: '100vh',
     backgroundImage:
       "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
     ...theme.applyStyles("dark", {
@@ -61,203 +68,219 @@ export default function HomePage(props: { disableCustomTheme?: boolean }) {
   return (
     // <Box height={'100%'} width="100%">
     <AboutMeContainer >
-      <Container maxWidth="lg">
-        <Card variant="outlined" sx={{ display: "flex" }}>
-          <Box
-            sx={{ display: "flex", flexDirection: "column", minWidth: "25%", justifyContent: 'center' }}
-          >
-            <CardContent sx={{ display: "block" }}>
-              <Avatar
-                alt="Jan de Voor"
-                src={avatarImg}
-                sx={{
-                  width: 150,
-                  height: 150,
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                }}
-              />
-              <Typography component="div" variant="h4" align="center">
-                Jan Paul de Voor
-              </Typography>
-              <Typography variant="subtitle1" component="div" align="center">
-                Graduate Research Assistant
-              </Typography>
-              <Typography variant="subtitle1" component="div" align="center">
-                Clemson University
-              </Typography>
+      <Container maxWidth="lg" >
+        <Card variant="outlined" sx={{ display: 'flex' }}>
+          <Grid container >
+            <Grid size={{ xs: 12, sm: 4 }}>
               <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  pl: 1,
-                  pb: 1,
-                  mt: 1,
-                  justifyContent: "space-evenly",
-                }}
+                sx={{ display: "flex", flexDirection: "column", minWidth: "25%", justifyContent: 'center' }}
               >
-                <Tooltip title="Google Scholar">
-                  <IconButton
-                    aria-label="google-scholar"
-                    href="https://scholar.google.com/citations?hl=en&user=O0eGg0wAAAAJ"
+                <CardContent sx={{ display: "block" }}>
+                  <Avatar
+                    alt="Jan de Voor"
+                    src={avatarImg}
+                    sx={{
+                      width: 150,
+                      height: 150,
+                      marginLeft: "auto",
+                      marginRight: "auto",
+                    }}
+                  />
+                  <Typography component="div" variant="h4" align="center">
+                    Jan Paul de Voor
+                  </Typography>
+                  <Typography variant="subtitle1" component="div" align="center">
+                    Graduate Research Assistant
+                  </Typography>
+                  <Typography variant="subtitle1" component="div" align="center">
+                    Clemson University
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      pl: 1,
+                      pb: 1,
+                      mt: 1,
+                      justifyContent: "space-evenly",
+                    }}
                   >
-                    <Icon className="fa-brands fa-google-scholar" />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="GitHub">
-                  <IconButton
-                    aria-label="github"
-                    href="https://github.com/JanPauldeVoor"
-                  >
-                    <GitHub />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="Linked-In">
-                  <IconButton
-                    aria-label="linkedin"
-                    href="https://www.linkedin.com/in/jan-de-voor"
-                  >
-                    <LinkedIn />
-                  </IconButton>
-                </Tooltip>
+                    <Tooltip title="Google Scholar">
+                      <IconButton
+                        aria-label="google-scholar"
+                        href="https://scholar.google.com/citations?hl=en&user=O0eGg0wAAAAJ"
+                      >
+                        <Icon className="fa-brands fa-google-scholar" />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="GitHub">
+                      <IconButton
+                        aria-label="github"
+                        href="https://github.com/JanPauldeVoor"
+                      >
+                        <GitHub />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Linked-In">
+                      <IconButton
+                        aria-label="linkedin"
+                        href="https://www.linkedin.com/in/jan-de-voor"
+                      >
+                        <LinkedIn />
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
+                </CardContent>
               </Box>
-            </CardContent>
-          </Box>
-          <Box sx={{ alignItems: "center", mt: 1, ml: 1, pl: 1, pb: 2 }}>
-            <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-              <Typography variant="h3" fontWeight={"bold"} sx={{ mb: 1 }}>
-                About Me
-              </Typography>
-              <Typography variant="h6" fontWeight={"normal"}>
-                Hello! I am a PhD student at Clemson University studying
-                Computer Science, and I am working under Dr. Mert Pesé in the
-                TigerSec lab. My research focus is Automotive embedded systems
-                security, focusing on Software Defined Vehicles and Zonal
-                Architecture. I’ve gained experience in embedded systems
-                software and hardware design through both academic and personal
-                projects. I have a passion for designing and 3D printing custom
-                components for more efficient and attractive solutions in my
-                work.
-              </Typography>
-              <Stack direction="row" spacing={2} marginTop={2}>
-                <Box sx={{ alignItems: "center" }}>
-                  <Typography
-                    variant="h4"
-                    sx={{ textDecoration: "underline", mb: 0 }}
-                  >
-                    Interests
+            </Grid>
+            <Grid size={{ xs: 12, sm: 8 }}>
+              <Box sx={{ alignItems: "center", mt: 1, ml: 1, pl: 1, pb: 2 }}>
+                <CardContent sx={{ display: "flex", flexDirection: "column" }}>
+                  <Typography variant="h3" fontWeight={"bold"} sx={{ mb: 1 }}>
+                    About Me
                   </Typography>
-                  <List sx={{ listStyleType: "disc", mt: 0, pl: 2 }}>
-                    <ListItem
-                      sx={{ display: "list-item", pb: 0 }}
-                    >
-                      <ListItemText>
-                        Automotive Embedded Systems Security
-                      </ListItemText>
-                    </ListItem>
-                    <ListItem
-                      sx={{ display: "list-item", pb: 0 }}
-                    >
-                      <ListItemText>
-                        Secure Embedded Systems
-                      </ListItemText>
-                    </ListItem>
-                    <ListItem
-                      sx={{ display: "list-item" }}
-                    >
-                      <ListItemText>
-                        Secure Safety-Critcal Networking
-                      </ListItemText>
-                    </ListItem>
-                  </List>
-                </Box>
-                <Box sx={{ display: "block" }}>
-                  <Typography
-                    variant="h4"
-                    sx={{ textDecoration: "underline", mb: 0 }}
-                  >
-                    Education
+                  <Typography variant="h6" fontWeight={"normal"}>
+                    Hello! I am a PhD student at Clemson University studying
+                    Computer Science, and I am working under Dr. Mert Pesé in the
+                    TigerSec lab. My research focus is Automotive embedded systems
+                    security, focusing on Software Defined Vehicles and Zonal
+                    Architecture. I’ve gained experience in embedded systems
+                    software and hardware design through both academic and personal
+                    projects. I have a passion for designing and 3D printing custom
+                    components for more efficient and attractive solutions in my
+                    work.
                   </Typography>
-                  <List sx={{ width: "100%", display: 'block', pl: 0, ml: 0 }}>
-                    <ListItem sx={{ pl: 0 }}>
-                      <ListItemIcon sx={{ minWidth: '2.5rem' }}  >
-                        <School />
-                      </ListItemIcon>
-                      <ListItemText primary={"PhD in Computer Science"} secondary="Clemson University, Expected December 2027" />
-                    </ListItem>
-                    <ListItem sx={{ pl: 0 }}>
-                      <ListItemAvatar sx={{ minWidth: '2.5rem' }}>
-                        <School />
-                      </ListItemAvatar>
-                      <ListItemText primary="B.S. Computer Science, Minor in Electrical Engineering" secondary="Clemson University, 2023" />
-                    </ListItem>
-                  </List>
-                </Box>
-              </Stack>
-            </CardContent>
-          </Box>
+                  <Grid container>
+                    <Grid size={{ xs: 12, md: 6 }}>
+                      <Box sx={{ alignItems: "center" }}>
+                        <Typography
+                          variant="h4"
+                          sx={{ textDecoration: "underline", mb: 0 }}
+                        >
+                          Interests
+                        </Typography>
+                        <List sx={{ listStyleType: "disc", mt: 0, pl: 2 }}>
+                          <ListItem
+                            sx={{ display: "list-item", pb: 0 }}
+                          >
+                            <ListItemText>
+                              Automotive Embedded Systems Security
+                            </ListItemText>
+                          </ListItem>
+                          <ListItem
+                            sx={{ display: "list-item", pb: 0 }}
+                          >
+                            <ListItemText>
+                              Secure Embedded Systems
+                            </ListItemText>
+                          </ListItem>
+                          <ListItem
+                            sx={{ display: "list-item" }}
+                          >
+                            <ListItemText>
+                              Secure Safety-Critcal Networking
+                            </ListItemText>
+                          </ListItem>
+                        </List>
+                      </Box>
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 6 }}>
+                      <Box sx={{ display: "block" }}>
+                        <Typography
+                          variant="h4"
+                          sx={{ textDecoration: "underline", mb: 0 }}
+                        >
+                          Education
+                        </Typography>
+                        <List sx={{ width: "100%", display: 'block', pl: 0, ml: 0 }}>
+                          <ListItem sx={{ pl: 0 }}>
+                            <ListItemIcon sx={{ minWidth: '2.5rem' }}  >
+                              <School />
+                            </ListItemIcon>
+                            <ListItemText primary={"PhD in Computer Science"} secondary="Clemson University, Expected December 2027" />
+                          </ListItem>
+                          <ListItem sx={{ pl: 0 }}>
+                            <ListItemAvatar sx={{ minWidth: '2.5rem' }}>
+                              <School />
+                            </ListItemAvatar>
+                            <ListItemText primary="B.S. Computer Science, Minor in Electrical Engineering" secondary="Clemson University, 2023" />
+                          </ListItem>
+                        </List>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Box>
+            </Grid>
+          </Grid>
         </Card>
         <Card variant="outlined" sx={{ display: "flex", mt: 1 }}>
           <Stack display={'flex'} spacing={2} textAlign={'center'} justifyContent={'center'} width='100%'>
             <Box>
               <Typography variant='h3' fontWeight={'bold'}>
-                Project Showcase
+                Featured Projects
               </Typography>
             </Box>
-            <Box sx={{ width: '100%', display: 'flex', justifyContent: 'space-evenly' }}>
-              <Card sx={{ maxWidth: 250 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component='img'
-                    image={powersynth}
-                    alt='powersynth image'
-                    sx={{ mb: 1 }}
-                  />
-                  <Typography gutterBottom variant='h5' component='div'>
-                    Power-Synth
-                  </Typography>
-                  <Typography variant='body2' sx={{ color: 'text.secondary' }}>
-                    Part Synthesizer and Part Nintendo Powerglove featuring a custom designed PCB.
-                  </Typography>
-                </CardActionArea>
-              </Card>
-              <Card sx={{ maxWidth: 380 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component='img'
-                    image={dash}
-                    alt='dash image'
-                    sx={{ mb: 1 }}
-                  />
-                  <Typography gutterBottom variant='h5' component='div'>
-                    Formula SAE Dash
-                  </Typography>
-                  <Typography variant='body2' sx={{ color: 'text.secondary' }}>
-                    Real-time system monitoring for the Clemson Formula SAE team.
-                  </Typography>
-                </CardActionArea>
-              </Card>
-              <Card sx={{ maxWidth: 320 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component='img'
-                    image={zat}
-                    alt='zat image'
-                    sx={{ mb: 1 }}
-                  />
-                  <Typography gutterBottom variant='h5' component='div'>
-                    Zonal Architecture Testbed
-                  </Typography>
-                  <Typography variant='body2' sx={{ color: 'text.secondary' }}>
-                    Open-source hardware-agnostic testbed for Software Defined Vehicles.
-                  </Typography>
-                </CardActionArea>
-              </Card>
-            </Box>
+            <Grid container spacing={2} >
+              <Grid size={{ xs: 12, md: 4.8 }}>
+                <Card >
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      image={dash}
+                      alt='dash image'
+                      sx={{ mb: 1 }}
+                    />
+                    <Typography gutterBottom variant='h5' component='div'>
+                      Formula SAE Dash
+                    </Typography>
+                    <Typography variant='body2' sx={{ color: 'text.secondary' }}>
+                      Real-time system monitoring for the Clemson Formula SAE team.
+                    </Typography>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+              <Grid size={{ xs: 12, md: 3 }} >
+                <Card >
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      image={powersynth}
+                      alt='powersynth image'
+                      sx={{ mb: 1 }}
+                    />
+                    <Typography gutterBottom variant='h5' component='div'>
+                      Power-Synth
+                    </Typography>
+                    <Typography variant='body2' sx={{ color: 'text.secondary' }}>
+                      Part Synthesizer and Part Nintendo Powerglove featuring a custom designed PCB.
+                    </Typography>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+
+              <Grid size={{ xs: 12, md: 4.1 }}>
+                <Card >
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      image={zat}
+                      alt='zat image'
+                      sx={{ mb: 1 }}
+                    />
+                    <Typography gutterBottom variant='h5' component='div'>
+                      Zonal Architecture Testbed
+                    </Typography>
+                    <Typography variant='body2' sx={{ color: 'text.secondary' }}>
+                      Open-source hardware-agnostic testbed for Software Defined Vehicles.
+                    </Typography>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            </Grid>
           </Stack>
         </Card>
-      </Container>
+      </Container >
     </AboutMeContainer >
-    // </Box>
   );
 }
--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,10 +1,13 @@
+import * as React from 'react';
 import { styled } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-
+import { IconButton, Menu, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box'
 const pages = ["Home", "Projects", "Research", "Publications", "Contact"];
 
 // Center the pages relative to the width of the device
@@ -22,6 +25,17 @@ const StyledPages = styled("div")(({ theme }) => ({
 }));
 
 export default function NavToolbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -32,7 +46,7 @@ export default function NavToolbar() {
             component="a"
             href="/"
             sx={{
-              display: { xs: "none", md: "flex" },
+              display: { xs: 'none', md: 'flex' },
               fontFamily: "monospace",
               fontWeight: 1000,
               letterSpacing: ".05rem",
@@ -42,22 +56,86 @@ export default function NavToolbar() {
           >
             Jan de Voor
           </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size='large'
+              aria-label='navigation menu'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom', horizontal: 'left'
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top', horizontal: 'left'
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  component="a"
+                  href={page === "Home" ? "/" : "/" + page}
+
+                >
+                  <Typography
+                    sx={{
+                      textAlign: 'center'
+                    }}>
+                    {page}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h2"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              fontFamily: "monospace",
+              fontWeight: 1000,
+              letterSpacing: ".05rem",
+              color: "inherit",
+              textDecoration: "none",
+              flexGrow: 1
+            }}
+          >
+            Jan de Voor
+          </Typography>
           <StyledPages>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                href={page === "Home" ? "/" : "/" + page}
-                sx={{
-                  display: { xs: "none", md: "flex" },
-                  color: "white",
-                  fontWeight: "bold",
-                  fontSize: "h4.fontSize",
-                  textDecoration: "underline",
-                }}
-              >
-                {page}
-              </Button>
-            ))}
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  href={page === "Home" ? "/" : "/" + page}
+                  sx={{
+                    my: 2,
+                    display: { xs: "none", md: "flex" },
+                    color: "white",
+                    fontWeight: "bold",
+                    fontSize: "h4.fontSize",
+                    textDecoration: "underline",
+                  }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
           </StyledPages>
         </Toolbar>
       </Container>
